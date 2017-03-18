5dd9bf988891be236ad9889cc2006d8a89249d87
andresmtzc <andresmtzc@gmail.com>
2017-03-17 18:23:37 -0600

    5
    5

--- html_form_send.php
+++ html_form_send.php
@@ -9,10 +9,10 @@ if(isset($_POST['email'])) {
      
     function died($error) {
         // your error code can go here
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
-        echo $error."<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
+        echo "We are sorry, we found the following error(s): ";
+        echo "<br /><br />";
+        echo $error."<br />";
+        echo "Please go back and try again.<br /><br />";
         die();
     }
      
@@ -34,17 +34,17 @@ if(isset($_POST['email'])) {
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
   if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+    $error_message .= 'The "Email Address" you entered does not appear to be valid.<br />';
   }
     $string_exp = "/^[A-Za-z .'-]+$/";
   if(!preg_match($string_exp,$first_name)) {
-    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+    $error_message .= 'The "First Name" you entered does not appear to be valid.<br />';
   }
   if(!preg_match($string_exp,$last_name)) {
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
+    $error_message .= 'The "Last Name" you entered does not appear to be valid.<br />';
   }
   if(strlen($comments) < 2) {
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+    $error_message .= 'The "Message" you entered does not appear to be valid.<br />';
   }
   if(strlen($error_message) > 0) {
     died($error_message);
@@ -70,6 +70,6 @@ $headers = 'From: '.$email_from."\r\n".
 @mail($email_to, $email_subject, $email_message, $headers);  
 
 
-header('Location: http://arquidromo.com/'); //Replace email-success.php with the page you want them to be redirected to!
+header('Location: http://arquidromo.com/thanks.html'); //Replace email-success.php with the page you want them to be redirected to!
 }
 ?>
