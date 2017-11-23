3bb0ed650a638d5c30a356f92f351252ba47c01f
andresmtzc <andresmtzc@gmail.com>
2017-11-23 08:24:56 -0600

    1
    1

--- html_form_send.php
+++ html_form_send.php
@@ -72,7 +72,7 @@ $headers = 'From: '.$email_from."\r\n".
 'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);  
 
-
-header('Location: http://arquidromo.com/thanks.html'); //Replace email-success.php with the page you want them to be redirected to!
+//Redirect user to another page
+header('Location: http://arquidromo.com/new'); //Replace email-success.php with the page you want them to be redirected to!
 }
 ?>
